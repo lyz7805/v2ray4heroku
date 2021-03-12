877cfa6552bff2a5eb65d19634a8301926c199d9
lyz7805 <lyz7805@gmail.com>
2021-03-12 17:21:38 +0800

    初始化项目

---
+++ v2ray.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# Set ARG
+ARCH="64"
+
+# Download files
+V2RAY_FILE="v2ray-linux-${ARCH}.zip"
+DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
+echo "Downloading binary file: ${V2RAY_FILE}"
+echo "Downloading binary file: ${DGST_FILE}"
+
+TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
+wget -O ${PWD}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} > /dev/null 2>&1
+wget -O ${PWD}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download binary file: ${V2RAY_FILE} ${DGST_FILE}" && exit 1
+fi
+echo "Download binary file: ${V2RAY_FILE} ${DGST_FILE} completed"
+
+# Check SHA512
+LOCAL=$(openssl dgst -sha512 v2ray.zip | sed 's/([^)]*)//g')
+STR=$(cat v2ray.zip.dgst | grep 'SHA512' | head -n1)
+
+if [ "${LOCAL}" = "${STR}" ]; then
+    echo " Check passed" && rm -fv v2ray.zip.dgst
+else
+    echo " Check have not passed yet " && exit 1
+fi
+
+# Prepare
+echo "Prepare to use"
+unzip v2ray.zip && chmod +x v2ray v2ctl
+mv v2ray v2ctl /usr/bin/
+mv geosite.dat geoip.dat /usr/local/share/v2ray/
+# mv config.json /etc/v2ray/config.json
+
+# Set config file
+cat << EOF > /etc/v2ray/config.json
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "alterId": 64
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+# Clean
+rm -rf ${PWD}/*
+echo "Done"
